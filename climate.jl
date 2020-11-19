a371e319b2bb9b6e530463c0e5191630985f85f4
Eric Larson <elarson@akamai.com>
2020-11-19 16:32:24 -0500

    Add regression

--- climate.jl
+++ climate.jl
@@ -6,11 +6,13 @@ using HTTP;
 using JSON;
 using DataFrames;
 using Plots;
+using SingularSpectrumAnalysis;
+using GLM;
 
 function getWebForm()
    d0 = Dict{String,Any}(
         "sdate" => "1873-01-01",
-        "edate" => "2019-12-31",
+        "edate" => "2020-10-31",
         "sid"   => "mspthr",
         "elems" => Any[
           Dict{String,Any}("duration" => "dly","name" => "maxt","add" => "t","interval" => "dly"),
@@ -27,6 +29,7 @@ end
 
 function getData(params)
    resp = HTTP.request("POST", "https://data.rcc-acis.org/StnData", ["Content-Type" => "application/x-www-form-urlencoded"], params) 
+   println("Data retrieved")
    bodystr = String(resp.body);
    body = JSON.parse(bodystr);
 end
@@ -37,14 +40,51 @@ function toDf(body::Dict{String,Any})
    df = body["data"] |> DataFrame |> transposeDf;
 end
 
-params = getWebForm();
-body = getData(params);
-df = toDf(body);
+function getHighs()
+   params = getWebForm();
+   body = getData(params);
+   df = toDf(body);
+   df.high = map(x->parse(Float64,x[1]), df.high);
+end
 
-df.high = map(x->parse(Int,x[1]), df.high);
+function plotHighs(high)
+   plot(high, label = "high")
+   file = "/tmp/climate";
+   savefig(file);
+   println("File saved as " * file * ".png");
+   println("On Mac, view with \"open " * file * ".png;\"");
+end
 
-plot(df.high, label = "high")
-file = "/tmp/climate";
-savefig(file);
-println("File saved as " * file * ".png");
-println("On Mac, view with \"open " * file * ".png;\"");
+function getTrend(high10)
+   yt, ys = analyze(high10, 365, robust=true) 
+   dfyt = DataFrame(temp = yt)
+   seq0 = [1:size(dfyt)[1]][1]
+   insert!(dfyt,2,seq0,:day) 
+   return dfyt
+end 
+
+function getLinearRegression(dfyt)
+   ols = lm(@formula(temp ~ day), dfyt)
+   fit = predict(ols)
+end
+
+function plotRegression(fit, temp)
+   println("Begin plotting")
+   plot(fit,label="fit")
+   ytrounded = map(x->round(x,digits=3), temp)
+   plot!(ytrounded,label="yt")
+   file = "/tmp/climatetrend";
+   savefig(file);
+   println("File saved as " * file * ".png");
+   println("On Mac, view with \"open " * file * ".png;\"");
+end
+
+function plotRegression(high)
+   dfyt = getTrend(high)
+   fit = getLinearRegression(dfyt)   
+   plotRegression(fit, dfyt.temp) 
+end 
+
+high = getHighs()
+#plotHighs(high)
+plotRegression(high)
